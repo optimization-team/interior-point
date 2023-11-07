2a99e5c4e514ac867b85f2deaf05874c480b4b57
Alexandra Vabnits <a.vabnits@innopolis.university>
2023-11-07 19:37:18 +0300

    fix main + test

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ from Function import Function
 def main():
 
     print("InteriorPoint")
-    function, matrix, b, approximation, initial_solution = parse_file("inputs/input1.txt", initial_point=True)
+    function, matrix, b, approximation, initial_solution = parse_file("inputs/input7.txt", initial_point=True)
     interior_point = InteriorPoint(function, matrix, b, initial_solution, approximation, True)
+    np.set_printoptions(precision=approximation)
 
     print(interior_point)
     solution = interior_point.optimize()
@@ -18,8 +19,10 @@ def main():
     print("Simplex")
     simplex = Simplex(function, matrix, b, approximation, True)
     print(simplex)
+
     solution = simplex.optimise(print_iterations=False)
     print(solution)
+
 
 
 if __name__ == "__main__":
