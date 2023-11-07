6749986d64d41849c45dbdac71d06961f6311276
Hexy00123 <r.izmajlov2018@yandex.ru>
2023-11-07 22:01:32 +0300

    Revert "Correct display with the specified accuracy"
    This reverts commit a6f13192fd1de286c5a0ea0d7fe24cb96224cddd.

--- InteriorPoint.py
+++ InteriorPoint.py
@@ -3,6 +3,9 @@ from dataclasses import dataclass
 from Function import Function
 import numpy as np
 
+from Exceptions import (AlternatingOptima, InfeasibleSolution,
+                        InvalidRightVector, DivergenceException)
+
 
 @dataclass
 class Solution:
@@ -25,41 +28,40 @@ class Solution:
             f"Optimal value: {self.opt}"
         )
 
-    """
-    Interior point method implementation for calculating the optimal value of input vector for a given function and constraints.
-    Attributes
-    ----------
-        Initial variables:
-        ------------------
-        function: Function
-
-        constraints_matrix: np.array
-
-        C: np.array
-            function with slack variables
-
-        A: np.array
-            matrix of constraints (assumed that all given in the form of inequalities) with slack variables
-
-        b: np.array
-            right hand side column vector (size n x 1)
-
-        eps: int
-            approximation of an answer (number of digits after comma)
-
-        m: int
-            number of constraints
-
-        n: int
-            number of variables
-            
-        alpha: float
-            distance that could be moved before the feasible region is left.
-            0 < alpha < 1
-"""
-
 
 class InteriorPoint:
+    """
+        Interior point method implementation for calculating the optimal value of input vector for a given function and constraints.
+        Attributes
+        ----------
+            Initial variables:
+            ------------------
+            function: Function
+
+            constraints_matrix: np.array
+
+            C: np.array
+                function with slack variables
+
+            A: np.array
+                matrix of constraints (assumed that all given in the form of inequalities) with slack variables
+
+            b: np.array
+                right hand side column vector (size n x 1)
+
+            eps: int
+                approximation of an answer (number of digits after comma)
+
+            m: int
+                number of constraints
+
+            n: int
+                number of variables
+
+            alpha: float
+                distance that could be moved before the feasible region is left.
+                0 < alpha < 1
+    """
     def __init__(
             self,
             C: Function,
@@ -74,8 +76,10 @@ class InteriorPoint:
         self.initial_solution = initial_solution
         self.function = C
         self.constraints_matrix = A
-        self.C = np.array(C.coefficients)
-        self.A = A
+        # self.C = np.array(C.coefficients)
+        # self.A = A
+        self.C = np.hstack((np.array(C.coefficients), np.zeros(A.shape[0])))
+        self.A = np.hstack((A, np.identity(A.shape[0])))
         self.b = b
         self.eps = eps
         self.epsilon = 1 / (10 ** eps)
@@ -83,6 +87,11 @@ class InteriorPoint:
         self.iteration = 0
         self.m, self.n = self.A.shape
         self.alfa = alpha
+        self.check_inputs()
+
+    def check_inputs(self):
+        if any(map(lambda x: x < 0, self.b)):
+            raise InvalidRightVector(self.b)
 
     def build_initial_solution(self) -> np.array:
         pass
@@ -104,8 +113,15 @@ class InteriorPoint:
             x_w = np.transpose(np.array([1.] * self.n) + self.alfa / v * c_p)
             next_solution = np.ndarray.tolist(np.transpose(d @ x_w))[0]
             accuracy = np.linalg.norm(np.subtract(self.solution, next_solution), ord=2)
+            if round(self.function(next_solution[:self.n - self.m]), self.eps) == round(self.function(self.solution[:self.n - self.m]), self.eps):
+                raise AlternatingOptima(Solution(next_solution[:self.n - self.m], round(self.function(self.solution[:self.n - self.m]), self.eps)))
             self.solution = next_solution
-        return Solution([round(i, self.eps) for i in self.solution], round(self.function(self.solution), self.eps))
+
+            if any(map(lambda x: x < 0, self.solution[:self.n - self.m])):
+                raise DivergenceException(self.solution[:self.n - self.m])
+
+        return Solution(self.solution[:self.n - self.m],
+                        round(self.function(self.solution[:self.n - self.m]), self.eps))
 
     def __str__(self):
         to_maximize = "max" if self.to_maximize else "min"
@@ -115,16 +131,19 @@ class InteriorPoint:
         constraints = constraints[:-1]
         constraints = constraints.replace("[[", "|").replace("]]", "|")
         approximation = f"Approximation: {self.eps}"
-        return f"LPP:\n{self.function} -> {to_maximize}\n{constraints}\n{approximation}\n"
+        alpha = f"Alpha: {self.alfa}"
+        return f"LPP:\n{self.function} -> {to_maximize}\n{constraints}\n{approximation}\n{alpha}\n"
 
 
 if __name__ == "__main__":
-    function = Function([1, 1, 0, 0])
-    constraints = np.matrix([[2, 4, 1, 0], [1, 3, 0, -1]])
-    b = np.array([16.0, 9.0])
-    initial_solution = np.array([1 / 2, 7 / 2, 1, 2])
-    approximation = 3
-    interior_point = InteriorPoint(function, constraints, b, initial_solution, approximation, True)
+    from input_parser import parse_file, parse_test
+
+    function, matrix, b, approximation, initial_solution = parse_file("inputs/input6.txt", initial_point=True)
+    # function, matrix, b, approximation, x, fun, initial_solution = parse_test("tests/test6.txt", initial_point=True)
+    # initial_solution1 = np.array([1 / 2, 7 / 2, 1, 2])
+    # parse initial solution
+    interior_point = InteriorPoint(function, matrix, b, initial_solution, approximation, True)
+
     print(interior_point)
     solution = interior_point.optimize()
     print(solution)
